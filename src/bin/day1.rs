1431df3b33324558f20804a2ea299141d3b0e4e1
Jacfger <ncfung@connect.ust.hk>
2023-12-02 18:02:45 +0800

    day1 completed, rust is hard

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,8 +1,13 @@
-fn read_inputs() {
-    
-}
-
+use aoc_2023::read_inputs;
 
 fn main() {
-    println!("Hello, world!");
+    let mut count: u32 = 0;
+    let inputs = read_inputs("day1.txt");
+    for line in inputs {
+        let first_num = line.chars().find(|c| c.is_numeric()).unwrap();
+        let last_num = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+        let num = first_num.to_digit(10).unwrap() * 10 + last_num.to_digit(10).unwrap();
+        count += num;
+    }
+    println!("Total: {}", count);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
 
+pub fn read_inputs<P: AsRef<Path>>(filename: P) -> Vec<String> {
+    let file = File::open(filename).expect("Failed to read file");
+    return BufReader::new(file).lines().map(|line| line.unwrap()).collect();
+}
